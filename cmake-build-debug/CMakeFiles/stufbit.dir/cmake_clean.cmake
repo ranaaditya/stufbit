ac53f0286c4c0371ea866b3f443de9a116de3100
ranaaditya <mail2aditya.rana98@gmail.com>
2022-07-29 23:34:41 +0530

    update basic project structure

---
+++ cmake-build-debug/CMakeFiles/stufbit.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/stufbit.dir/main.cpp.o"
+  "stufbit"
+  "stufbit.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/stufbit.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/stufbit.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for stufbit.
+# This may be replaced when dependencies are built.
